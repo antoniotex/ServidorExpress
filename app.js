c6a9aa4820e5e1dfec0961fda76fc99fda33ed9c
Antonio Carlos <acm.skt@gmail.com>
2018-01-05 05:05:45 -0200

    Atualizado para a linguagem de modelagem EJS

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
+//Recupera biblioteeca express e executa função recuperada
 var express = require('express');
-
 var app = express();
+// fim
 
-app.get('/', function(req, res){
-	res.send('<html><head><title>Ola Mundo</title></head><bodybody style="color:grey;"><h1>Portal de noticias</h1></body></html>');
-});
+//informa para o EXPRESS que o EJS será o motor de views
+app.set('view engine', 'ejs')
 
 app.get('/tecnologia', function(req, res){
-	res.send('<html><head><title>Ola Mundo</title></head><body style="color:red;"><h1>Portal de tecnologia</h1></body></html>');
+	res.render('secao/tecnologia');
+});
+
+app.get('/moda', function(req, res){
+	res.render('secao/moda');
 });
 
 app.get('/carros', function(req, res){
-	res.send('<html><head><title>Ola Mundo</title></head><body style="color:green;"><h1>Portal de carros</h1></body></html>');
+	res.render('secao/carros');
 });
 
+
+//Fica escutando requisição da porta 3000 e foi passado uma função de callback para confirmar que o servidor subiu
 app.listen(3000, function(){
 	console.log('Servidor rodando com Express');
 })
