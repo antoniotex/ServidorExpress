68cc674b55cf78cf4a1f03109fcabf896c372dda
Antonio Carlos <acm.skt@gmail.com>
2018-01-09 22:58:47 -0200

    Add consign on database

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-
 var app = require('./config/server');
-
-
-//var rotaNoticias = require('./app/routes/noticias');
-//rotaNoticias(app);
-
-//var rotaHome = require('./app/routes/home');
-//rotaHome(app);
-
-//var rotaFormularioInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia');
-//rotaFormularioInclusaoNoticia(app);
 
 //Fica escutando requisição da porta 3000 e foi passado uma função de callback para confirmar que o servidor subiu
 app.listen(3000, function(){
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,15 @@
 var mysql = require('mysql');
 
-module.exports = function (){
-
+var connMySql = function(){
+		console.log('Connection db estabilshed');
 		return mysql.createConnection({
 			host: 'localhost',
 			user: 'root',
 			password: '',
 			database: 'portal_noticias'
 		});
+}
+
+module.exports = function (){
+	return connMySql;
 }
